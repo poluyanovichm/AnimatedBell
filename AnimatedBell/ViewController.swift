2ac9bc6ae8af4421ad7b2587a9f72a562dacbb30
mpoluyanovich <poluyanovich@mail.ru>
2022-12-30 09:15:02 +0300

    Initial Commit

---
+++ AnimatedBell/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimatedBell
+//
+//  Created by Mikhail on 30.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
